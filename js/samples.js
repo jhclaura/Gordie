8f8c713b70ff5eef86b05692d7bcc4e765ebb1be
Laura Chen <jhclaura@gmail.com>
2015-10-30 16:50:18 -0400

    add audios & pics

---
+++ js/samples.js
@@ -0,0 +1,120 @@
+// Adapted from
+// "Web Audio API Advanced Sound for Games and Interactive Apps"
+// By Boris Smus
+
+var samplesAllLoaded = false;
+
+function SoundsSample(context) {
+
+  var ctx = this;
+  var loader = new BufferLoader(context, 
+            [ '../audios/9_Midori.mp3', '../audios/9_Midori.mp3', '../audios/9_Midori.mp3',
+              '../audios/3_danOo.mp3', '../audios/4_laura.mp3', '../audios/5_Marianne.mp3',
+              '../audios/9_Midori.mp3', '../audios/9_Midori.mp3', '../audios/9_Midori.mp3',
+              '../audios/9_Midori.mp3', '../audios/10_shiffman.mp3', '../audios/9_Midori.mp3',
+              '../audios/9_Midori.mp3', '../audios/9_Midori.mp3', '../audios/14_Katherine.mp3',
+              '../audios/9_Midori.mp3', '../audios/16_nancy.mp3', '../audios/9_Midori.mp3',
+              '../audios/9_Midori.mp3', '../audios/9_Midori.mp3', '../audios/20_marlon.mp3',
+              '../audios/9_Midori.mp3', '../audios/22_sam.mp3', '../audios/23_oryan.mp3',
+              '../audios/andy.mp3', '../audios/gladys.mp3', '../audios/jason.mp3'   // #24-
+              ], onLoaded);
+
+  function onLoaded(buffers) {
+    ctx.buffers = buffers;
+    samplesAllLoaded = true;
+  };
+
+  loader.load();
+
+  this.isCompressed = false;
+}
+
+SoundsSample.prototype.trigger = function(index) {
+  if (typeof random == 'undefined') {
+    random = 0;
+  }
+  var time = context.currentTime;
+
+  var source = this.makeSource(this.buffers[index]);
+  source[source.start ? 'start' : 'noteOn'](time);
+}
+
+SoundsSample.prototype.trigger = function(index, volume) {
+  if (typeof random == 'undefined') {
+    random = 0;
+  }
+  var time = context.currentTime;
+
+  var source = this.makeSource(this.buffers[index], volume);
+  source[source.start ? 'start' : 'noteOn'](time);
+}
+
+SoundsSample.prototype.makeSource = function(buffer) {
+  var source = context.createBufferSource();
+  var gain = context.createGain();
+  gain.gain.value = 1;
+  source.buffer = buffer;
+  source.connect(gain);
+
+  if (this.isCompressed) {
+    var compressor = context.createDynamicsCompressor();
+    compressor.threshold.value = 10;
+    compressor.ratio.value = 20;
+    compressor.reduction.value = -20;
+    gain.connect(compressor);
+    compressor.connect(context.destination);
+  } else {
+    gain.connect(context.destination);
+  }
+  return source;
+};
+
+SoundsSample.prototype.makeSource = function(buffer, volume) {
+  var source = context.createBufferSource();
+  var gain = context.createGain();
+  gain.gain.value = volume;
+  source.buffer = buffer;
+  source.connect(gain);
+
+  if (this.isCompressed) {
+    var compressor = context.createDynamicsCompressor();
+    compressor.threshold.value = 10;
+    compressor.ratio.value = 20;
+    compressor.reduction.value = -20;
+    gain.connect(compressor);
+    compressor.connect(context.destination);
+  } else {
+    gain.connect(context.destination);
+  }
+  return source;
+};
+
+SoundsSample.prototype.triggerPanner = function(index, location) {
+  if (typeof random == 'undefined') {
+    random = 0;
+  }
+  var time = context.currentTime;
+
+  var source = this.makePannerSource(this.buffers[index], location);
+  source[source.start ? 'start' : 'noteOn'](time);
+}
+
+SoundsSample.prototype.makePannerSource = function(buffer, location) {
+  var source = context.createBufferSource();
+  var gain = context.createGain();
+  gain.gain.value = 20;
+  source.buffer = buffer;
+  source.connect(gain);
+
+  //Panner
+  var panner = context.createPanner();
+  gain.connect(panner);
+  panner.setPosition(location.x, location.y, location.z);
+  panner.connect(context.destination);
+
+  return source;
+};
+
+SoundsSample.prototype.toggleCompressor = function() {
+  this.isCompressed = !this.isCompressed;
+}
--- js/script.js
+++ js/script.js
@@ -90,28 +90,77 @@
 	}
 
 
-// adapted from vr.chromeExperience
-var camera, scene, renderer;
-var effect, controls;
-var renderElement, container;
+// Three.js
+	var camera, scene, renderer;
+	var effect, controls;
+	var renderElement, container;
 
-var ball, ballMat;
-var photos=[];
-var boxes=[];
-var time;
+	var ball, ballMat;
+	var photos=[];
+	var boxes=[];
+	var time;
 
-var clock = new THREE.Clock();
+	var clock = new THREE.Clock();
 
-var photoFileRoutes = ["images/R0010108.JPG", "images/R0010109.JPG", "images/R0010111.JPG", "images/R0010112.JPG"];
-var inTheZone, pastChange;
+	var photoFileRoutes = ["images/0_empty.JPG", "images/1_John.JPG", "images/2_Rosalie.JPG",
+						   "images/3_Dan.JPG", "images/4_Laura.JPG", "images/5_Marianne.JPG",
+						   "images/6_George.JPG", "images/7_Matt.JPG", "images/8_Tom.JPG",
+						   "images/9_Midori.JPG", "images/10_Shiffman.JPG", "images/11_John_Matt.JPG",
+						   "images/12_Gabriel.JPG", "images/13_Danny.JPG", "images/14_Katherine.JPG",
+						   "images/15_Gabe.JPG", "images/16_Nancy.JPG", "images/17_Shawn.JPG",
+						   "images/18_Mimi.JPG", "images/19_Lauren.JPG", "images/20_Marlon.JPG",
+						   "images/21_Pedro.JPG", "images/22_Sam.JPG", "images/23_Oryan.JPG"];
+	var inTheZone, pastChange;
+
+	var currentCamPos;
 //
+
+// WEB_AUDIO_API!
+	var context, bufferLoader, convolver, mixer;
+	var source, buffer, audioBuffer, gainNode, convolverGain;
+	var mediaStreamSource;
+	var samples = 1024;
+	var soundLoaded = false;
+	var mainVolume;
+
+	var sound_sweet = {};
+
+	//
+	window.AudioContext = (window.AudioContext || window.webkitAudioContext || null);
+	if(!AudioContext){
+		throw new Error("AudioContext not supported!");
+	}
+	context = new AudioContext();
+
+	//
+	var sample = new SoundsSample(context);
+	var sweetSource;
+
+	//
+	var vecZ = new THREE.Vector3(0,0,1);
+	var vecY = new THREE.Vector3(0,-1,0);
+	var sswM, sswX, sswY, sswZ;
+	var camM, camMX, camMY, camMZ;	
+
+///////////////////////////////////////////////////////////////
 
 init();
 animate();
 
+///////////////////////////////////////////////////////////////
+
 function init() {
+
+	// WEB_AUDIO_API
+	bufferLoader = new BufferLoader(
+    	context, ['../audios/Gordie_sweet.mp3'], 	// #0
+    			  finishedLoading
+    	);
+    bufferLoader.load();
+	//
 	time = Date.now();
 
+	// THREE.JS
 	renderer = new THREE.WebGLRenderer({
 		antialias: true,
 		alpha: true
@@ -143,15 +192,11 @@ function init() {
 	window.addEventListener('click', fullscreen, false);
 
 	///////////////////////////////////////////////////////////////
-
-	// var light = new THREE.HemisphereLight(0x777777, 0x000000, 1);
-	// scene.add(light);
-
 	var light = new THREE.DirectionalLight( 0xffffff, 1 );
 	light.position.set(1,1,1);
 	scene.add(light);
 
-	// test cubes
+	// test spheres
 	var geometry = new THREE.SphereGeometry(1);
 	var material = new THREE.MeshLambertMaterial({ color: 0x00ffff });
 	var mesh;
@@ -164,7 +209,7 @@ function init() {
 		}
 	}
 
-
+	//
 	var onTouchStart = function ( event ) {
 		// console.log("touch!");
 		picIndex++;
@@ -194,6 +239,58 @@ function init() {
 
 	//
 	animate();
+
+	// audio!
+	if(samplesAllLoaded)
+		sample.trigger(0, 1);
+}
+
+function finishedLoading(bufferList){
+
+	bufferStorage = bufferList;
+
+	mixer = context.createGain();
+	mainVolume = context.createGain();
+	mainVolume.connect(context.destination);
+	//
+
+	sound_sweet.source = context.createBufferSource();
+	sound_sweet.source.buffer = bufferList[0];
+	sound_sweet.source.loop = true;
+	sound_sweet.gainNode = context.createGain();
+	sound_sweet.gainNode.gain.value = 1;
+	sound_sweet.source.connect(sound_sweet.gainNode);
+
+	sound_sweet.panner = context.createPanner();
+	sound_sweet.gainNode.connect(sound_sweet.panner);
+	sound_sweet.panner.connect(mainVolume);
+
+	// start to PLAY!
+	// sound_empty.source.start(context.currentTime);
+	sound_sweet.source.start(context.currentTime);
+
+	//
+	// Sweet source
+	geometry = new THREE.SphereGeometry(2);
+	material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+	sweetSource = new THREE.Mesh(geometry, material);
+	sweetSource.position.set(0,0,5);
+	scene.add(sweetSource);
+
+	sswM = sweetSource.matrixWorld;
+	sswX = sswM.elements[12];
+	sswY = sswM.elements[13];
+	sswZ = sswM.elements[14];
+	sswM.elements[12] = sswM.elements[13] = sswM.elements[14] = 0;
+
+	vecZ.applyProjection(sswM);
+	vecZ.normalize();
+
+	sound_sweet.panner.setOrientation(vecZ.x, vecZ.y, vecZ.z);
+
+	sswM.elements[12] = sswX;
+	sswM.elements[13] = sswY;
+	sswM.elements[14] = sswZ;
 }
 
 function loadModelBall(model, material) {
@@ -254,8 +351,10 @@ function update(dt) {
 
 	// controls.update(dt);
 	controls.update(Date.now() - time);
+	currentCamPos = controls.position();
 
-	var conRot = controls.rotation().y%3;
+	var conRot = controls.rotation().y%Math.PI;
+	// console.log(conRot);
 
 	// once conRot is in the zone of -0.0 ~ -1.5
 	// picture: changes
@@ -272,6 +371,38 @@ function update(dt) {
 	}
 	// console.log(conRott);
 
+	// SOUND
+	context.listener.setPosition( currentCamPos.x, currentCamPos.y, currentCamPos.z );
+	if(sound_sweet.panner) {
+		sound_sweet.panner.setPosition( sweetSource.position.x, sweetSource.position.y, sweetSource.position.z );
+
+		// orientation
+		camM = controls.getObject().matrix;
+
+		camMX = camM.elements[12];
+		camMY = camM.elements[13];
+		camMZ = camM.elements[14];
+		camM.elements[12] = camM.elements[13] = camM.elements[14] = 0;
+
+		// Multiply the orientation vector by the world matrix of the camera.
+		var vec = new THREE.Vector3(0,0,1);
+		vec.applyProjection(camM);
+		vec.normalize();
+
+		// Multiply the up vector by the world matrix.
+		var up = new THREE.Vector3(0,-1,0);
+		up.applyProjection(camM);
+		up.normalize();
+
+		// Set the orientation and the up-vector for the listener.
+		context.listener.setOrientation(vec.x, vec.y, vec.z, up.x, up.y, up.z);
+
+		camM.elements[12] = camMX;
+		camM.elements[13] = camMY;
+		camM.elements[14] = camMZ;
+	}
+
+	//
 	time = Date.now();
 }
 
